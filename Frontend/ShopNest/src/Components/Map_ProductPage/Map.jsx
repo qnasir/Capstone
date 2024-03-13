7229ec0b4e4cee9bd6368f9489e7489d0fe59add
qnasir <qnasir575@gmail.com>
2024-03-14 00:07:54 +0530

    Added share feature to product page and did animation

--- Frontend/ShopNest/src/Components/Map_ProductPage/Map.jsx
+++ Frontend/ShopNest/src/Components/Map_ProductPage/Map.jsx
@@ -16,7 +16,7 @@ const BingMapsComponent = () => {
             const newMap = new window.Microsoft.Maps.Map('#map', {
                 credentials: import.meta.env.VITE_BING_CREDENTIALS_KEY, 
                 center: new window.Microsoft.Maps.Location(10.81802845, 76.26371002),
-                zoom: 10
+                zoom: 10,
             });
 
             var pin = new window.Microsoft.Maps.Pushpin(newMap.getCenter());
@@ -30,42 +30,42 @@ const BingMapsComponent = () => {
         };
     }, []);
 
-    useEffect(() => {
-        if (map && !searchManager) {
-            window.Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
-                setSearchManager(new window.Microsoft.Maps.Search.SearchManager(map));
-            });
-        }
-    }, [map, searchManager]);
+    // useEffect(() => {
+    //     if (map && !searchManager) {
+    //         window.Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
+    //             setSearchManager(new window.Microsoft.Maps.Search.SearchManager(map));
+    //         });
+    //     }
+    // }, [map, searchManager]);
 
-    const handleSearch = () => {
-        if (searchManager) {
-            geocodeQuery(searchInput);
-        }
-    };
+    // const handleSearch = () => {
+    //     if (searchManager) {
+    //         geocodeQuery(searchInput);
+    //     }
+    // };
 
-    const geocodeQuery = (query) => {
-        if (!searchManager) return;
+    // const geocodeQuery = (query) => {
+    //     if (!searchManager) return;
 
-        let searchRequest = {
-            where: query,
-            callback: function (r) {
-                if (r && r.results && r.results.length > 0) {
-                    var pin = new window.Microsoft.Maps.Pushpin(r.results[0].location);
-                    map.entities.push(pin);
+    //     let searchRequest = {
+    //         where: query,
+    //         callback: function (r) {
+    //             if (r && r.results && r.results.length > 0) {
+    //                 var pin = new window.Microsoft.Maps.Pushpin(r.results[0].location);
+    //                 map.entities.push(pin);
 
-                    map.setView({ bounds: r.results[0].bestView });
-                } else {
-                    alert("No results found.");
-                }
-            },
-            errorCallback: function (error) {
-                alert("Error occurred during search.", error);
-            }
-        };
+    //                 map.setView({ bounds: r.results[0].bestView });
+    //             } else {
+    //                 alert("No results found.");
+    //             }
+    //         },
+    //         errorCallback: function (error) {
+    //             alert("Error occurred during search.", error);
+    //         }
+    //     };
 
-        searchManager.geocode(searchRequest);
-    };
+    //     searchManager.geocode(searchRequest);
+    // };
 
     return (
         <div>
--- Frontend/ShopNest/src/Components/ProductPage/ProductPage.jsx
+++ Frontend/ShopNest/src/Components/ProductPage/ProductPage.jsx
@@ -1,63 +1,142 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProductPage.css'
 import image from '../Product/svg/download.jpeg'
 import SearchBar from '../Product/SearchBar/SearchBar'
 import Map from '../Map_ProductPage/Map'
 
 // React Icons
-// import { FaIndianRupeeSign } from "react-icons/fa6";
 import { FaRupeeSign } from "react-icons/fa";
 import { SlLocationPin } from "react-icons/sl";
+import { ShareSocial } from 'react-ionicons';
+
+
+// Social Media Buttons
+import {
+    FacebookShareButton,
+    TelegramShareButton,
+    TwitterShareButton,
+    WhatsappShareButton,
+    PinterestShareButton,
+  
+} from "react-share";
+
+
+//Social Media Icons
+import {
+    FacebookIcon,
+    TelegramIcon,
+    WhatsappIcon,
+    PinterestIcon,
+    XIcon,
+} from "react-share";
 
 
 function ProductPage() {
+
+    const shareURL = 'Website URl'
+
+    const [isToggle, setIsToggle] = useState(false);
+
+    const ToggleShare = () => {
+        setIsToggle(!isToggle);
+    };
+
     return (
         <>
             <div className='parent_div'>
                 <SearchBar style={{ "width": "500px" }} />
             </div>
-                <div className='product_page_container'>
+            <div className='product_page_container'>
 
-                    <div className="product">
-                        <div className="product_image">
-                            <img src={image} alt="" />
+                <div className="product">
+                    <div className="product_image">
+                        <img src={image} alt="" />
+                    </div>
+                    <div className="product_info">
+                        <div className="heading">
+
+                            <div className='name'>
+                                <p>GREEN CHEF COOKER</p>
+                            </div>
+
+                            <div className="logo">
+                                <div className={isToggle ? 'toggle active' : 'toggle'} onClick={ToggleShare}>
+                                    <ShareSocial />
+                                </div>
+
+                                <ul className="share_buttons">
+                                  
+                                        <li style={{ '--i': '0', '--clr': '#25D366' }} className={isToggle ? 'active' : ''}>
+                                                <WhatsappShareButton className='social_icon' url={shareURL}>
+                                                    <WhatsappIcon size={30} round={true} />
+                                                </WhatsappShareButton>
+                                        </li>
+
+                                        <li style={{ '--i': '1', '--clr': '#E60023' }} className={isToggle ? 'active' : ''}>
+                                                <PinterestShareButton className='social_icon' url={shareURL}>
+                                                    <PinterestIcon size={30} round={true} />
+                                                </PinterestShareButton>
+                                        </li>
+
+                                        <li style={{ '--i': '2', '--clr': '#0965FE' }} className={isToggle ? 'active' : ''}>
+                                                <FacebookShareButton className='social_icon' url={shareURL}>
+                                                    <FacebookIcon size={30} round={true} />
+                                                </FacebookShareButton>
+                                        </li>
+
+                                        <li style={{ '--i': '3', '--clr': '#000000' }} className={isToggle ? 'active' : ''}>
+                                                <TwitterShareButton className='social_icon' url={shareURL}>
+                                                    <XIcon size={30} round={true} />
+                                                </TwitterShareButton>
+                                        </li>
+
+                                        <li style={{ '--i': '4', '--clr': '#25A3E3' }} className={isToggle ? 'active' : ''}>
+                                                <TelegramShareButton className='social_icon' url={shareURL}>
+                                                    <TelegramIcon size={30} round={true} />
+                                                </TelegramShareButton>
+                                        </li>
+
+        
+                                </ul>
+                            </div>
+
                         </div>
-                        <div className="product_info">
-                            <div className="heading">
-                                <p className='name'>GREEN CHEF COOKER</p>
+                        <p className='title'>Green Chef</p>
+                        <span className='price'>
+                            <FaRupeeSign className='price_icon' />
+                            <p>999</p>
+                        </span>
+                        <div className="discription">
+                            <p>Discription Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui aut, voluptatem voluptates optio alias aspernatur voluptate quidem veniam ab sequi odio iste temporibus assumenda molestias velit, quis dignissimos, debitis aliquam! Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                        <div className="footer">
+                            <div className="buttons">
+                                <button className='buy'>BUY NOW</button>
+                                <button className='wishlist'>ADD TO WISHLIST</button>
+                            </div>
+                            <div>
                                 <span className='location'>
                                     <SlLocationPin />
                                     <p>BH-2</p>
                                 </span>
                             </div>
-                            <p className='title'>Green Chef</p>
-                            <span className='price'>
-                                <FaRupeeSign className='price_icon' />
-                                <p>999</p>
-                            </span>
-                            <div className="discription">
-                                <p>Discription Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui aut, voluptatem voluptates optio alias aspernatur voluptate quidem veniam ab sequi odio iste temporibus assumenda molestias velit, quis dignissimos, debitis aliquam! Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                            </div>
-                            <div className="buttons">
-                                <button className='buy'>BUY NOW</button>
-                                <button className='wishlist'>ADD TO WISHLIST</button>
-                            </div>
                         </div>
                     </div>
-
-                    <div className="seller_info">
-                        <div className="seller_detail">
-                            <p className='post'>Posted By</p>
-                            <div className="seller_image">
-                                <img src={image} alt="" />
-                            </div>
-                            <p className='seller'>NASIR</p>
-                            <p className='verification'>VERIFIED</p>
-                        </div>
-                        <div className="map"><Map /></div>
-                    </div>
-
                 </div>
+
+                <div className="seller_info">
+                    <div className="seller_detail">
+                        <p className='post'>Posted By</p>
+                        <div className="seller_image">
+                            <img src={image} alt="" />
+                        </div>
+                        <p className='seller'>NASIR</p>
+                        <p className='verification'>VERIFIED</p>
+                    </div>
+                    <div className="map"><Map /></div>
+                </div>
+
+            </div>
         </>
     )
 }

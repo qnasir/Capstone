7daa1efdf8deb8308a195f3f24779d5761d6e07c
qnasir <qnasir575@gmail.com>
2024-04-05 09:39:10 +0530

    Deployed backend on vercel

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-const connectDb = require("./db");
+const connectDb = require("./db.js");
 const port = process.env.PORT || 3000;
 
 require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://shop-nest-seven.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: 'Content-Type',
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
 
 
 // Middleware for error catching
